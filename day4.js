d09c9128d8b083f759c31b2a5a680e4e6539dbc9
Tammie Chung <tammie.chung11@gmail.com>
2019-12-18 16:43:47 -0500

    Working on part 2

--- day4.js
+++ day4.js
@@ -6,19 +6,27 @@ const passwordCounter = (min, max) => {
   for (let i = min; i <= max; i++) {
     let password = Array.from(String(i), Number);
     let double = false;
+    let prevNums = {};
 
     if (password[5] >= password[4] 
       && password[4] >= password[3] 
       && password[3] >= password[2] 
       && password[2] >= password[1] 
       && password[1] >= password[0]) {
-        let prevNum;
+
         for (let num of password) {
+          let prevNum;
           if (num === prevNum) {
             double = true;
+            // if (prevNums[num]) {
+            //   prevNums[num]++;
+            // } else {
+            //   prevNums[num] = 1;
+            // }
           }
           prevNum = num;
         }
+
     }
     
     if (double) {
