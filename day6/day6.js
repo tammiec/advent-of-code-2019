deaf7230903307390e35ca6f32d7629b73bbb2a7
Tammie Chung <tammie.chung11@gmail.com>
2019-12-23 15:06:23 -0500

    set up counting function and example input

--- day6/day6.js
+++ day6/day6.js
@@ -5,6 +5,19 @@ const readFile = async () => {
 	data = data.split('\n');
 	console.log(data);
 	return data;
-}
+};
 
-const input = readFile();
+// const input = readFile();
+
+const input = 'COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L'.split(',');
+
+const countOrbits = orbits => {
+
+	let count = 0;
+	
+	return count;
+
+};
+
+countOrbits(input);
+
