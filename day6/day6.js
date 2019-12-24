23e65b525d7946059f44ea6847548aba8c25b116
Tammie Chung <tammie.chung11@gmail.com>
2019-12-23 21:28:07 -0500

    Added logic to determine number of orbiters - need to refactor to nested objects

--- day6/day6.js
+++ day6/day6.js
@@ -11,21 +11,75 @@ const readFile = async () => {
 
 const input = 'COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L'.split(',');
 
-const countOrbits = orbits => {
+// planet class
+class Planet {
+	constructor(name) {
+		this.name = name;
+		this.orbiters = [];
+	}
+	
+	addOrbiter(planet) {
+		this.orbiters.push(planet);
+	}
+	
+	get totalOrbiters() {
+		let total = 0;
+		for (let planet of this.orbiters) {
+			total += planet.totalOrbiters + 1;
+			console.log(`total for ${this.name} is ${total}`);
+		}
+		return total;
+	}
+}
 
-	let count = 0;
+// write a function to map out planets in orbit to tree structure
+
+const mapPlanets = (orbits) => {
+
+	const planetsInOrbit = [];
+
 	const splitOrbits = orbits.map(code => {
 		const relation = code.split(')');
-		return { inner: relation[0], outer: relation[1] }
+		return { inner: new Planet(relation[0]), outer: new Planet(relation[1]) }
 	});
-	console.log('orbits:', splitOrbits);
-	console.log('count:', count);
-	return count;
+	//console.log(splitOrbits);
+	
+	for (let pair of splitOrbits) {
+		pair.inner.addOrbiter(pair.outer);
+		planetsInOrbit.push(pair.inner);
+		planetsInOrbit.push(pair.outer);
+		console.log('pair', pair);
+	}
+	console.log('planets', planetsInOrbit);
+	return planetsInOrbit;
+};
+
+const mappedPlanets = mapPlanets(input);
+
+// get the parent node
+
+const getRootPlanet = (planets) => {
+
+	const orbiters = [];
+	
+	for (let planet of planets) {
+		if (planet.orbiters) {
+			planet.orbiters.forEach(orbiter => orbiters.push(orbiter.name));
+		};
+	}
+	
+	console.log('orbiters', orbiters);
+	
+	const root = planets.filter(planet => !orbiters.includes(planet.name));
+	console.log('root', root);
+	
+	return root[0];
 
 };
 
-// map out planets in orbit to tree structure 
-// traverse the tree - for each node traversed, count++
+const rootPlanet = getRootPlanet(mappedPlanets);
 
-countOrbits(input);
+// use get method to get total number of orbiters at the parent node
 
+console.log('number of orbiters', rootPlanet.totalOrbiters);
+console.log(rootPlanet);
