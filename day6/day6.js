f8425b15f38ffa8a439fd17564d2a148229996e3
Tammie Chung <tammie.chung11@gmail.com>
2019-12-24 22:59:23 -0500

    change structure to linked list instead

--- day6/day6.js
+++ day6/day6.js
@@ -2,20 +2,21 @@ const fs = require('fs');
 
 const readFile = () => {
 	let data = fs.readFileSync('./input.txt', 'utf-8');
-	data = data.split('\n');
-	console.log(data);
+  data = data.split('\n').map(code => code.split(')'));
+	// console.log(data);
 	return data;
 };
 
 // const input = readFile();
 
-const input = 'COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L'.split(',');
+const input = 'COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L'.split(',').map(code => code.split(')'));
 
 // planet class
 class Planet {
 	constructor(name) {
     this.name = name;
-    this.orbiters = [];
+    // this.orbiters = [];
+    this.parent;
 	}
 	
 	addOrbiter(planet) {
@@ -26,72 +27,86 @@ class Planet {
     this.parent = planet;
   }
 	
-	totalOrbiters(acc = 0) {
-    let total = acc;
-    for (let planet of this.orbiters) {
-      total++;
-      planet.totalOrbiters(total);
+	count() {
+    if (this.parent) {
+      return this.parent.count() + 1;
+    } else {
+      return 0;
     }
-    console.log(`total for ${this.name} is ${total}`);
-    return total;
 	}
 }
 
-// write a function to map out planets in orbit to tree structure
+// write a class to map out planets in orbit to tree structure
 
-const mapPlanets = (orbits) => {
+class PlanetMap {
 
-	const splitOrbits = orbits.map(code => {
-		const relation = code.split(')');
-		return { inner: new Planet(relation[0]), outer: new Planet(relation[1]) }
-  });
+  constructor(input) {
+    this.planets = {};
 
-  console.log('all planets', splitOrbits);
-
-  let planetsInOrbit = {};
-	
-	for (let pair of splitOrbits) {
-    const parent = pair.inner;
-    const orbiter = pair.outer;
-
-    if (planetsInOrbit[parent.name]) {
-      planetsInOrbit[parent.name].addOrbiter(orbiter);
-    } else {
-      parent.addOrbiter(orbiter);
-      planetsInOrbit[parent.name] = parent;
+    // add planet to map if it doesn't already exist
+    for (let [parent, orbiter] of input) {  
+      if (!this.planets[orbiter]) {
+        this.planets[orbiter] = new Planet(orbiter);
+      }
+      if (!this.planets[parent]) {
+        this.planets[parent] = new Planet(parent);
+      }
+      
+      // set parent-orbiter relationship
+      this.planets[orbiter].addParent(this.planets[parent]);
+  
     }
-
-    orbiter.addParent(parent);
-    planetsInOrbit[orbiter.name] = orbiter;
-
   }
-	console.log('planets', planetsInOrbit);
-	return planetsInOrbit;
+
+  getTotalOrbits() {
+    const total = Object.values(this.planets)
+      .map(planet => planet.count())
+      .reduce((a, b) => a + b);
+    return total;
+  };
+
 };
 
-const mappedPlanets = mapPlanets(input);
+// RUN CODE
+const map = new PlanetMap(input);
+console.log('map', map.planets);
+console.log('total', map.getTotalOrbits());
+
+// const mappedPlanets = mapPlanets(input);
+// console.log('mappedPlanets:', mappedPlanets)
+
+// const getTotalOrbits = (planets) => {
+//   const sum = Object.values(planets)
+//     .map(planet => planet.count())
+//     .reduce((a, b) => a + b);
+//   return sum;
+// };
+
+// const totalOrbits = getTotalOrbits(mappedPlanets);
+// console.log('totalOrbits:', totalOrbits)
+
 
 // get the parent node
 
-const getRootPlanet = (planets) => {
+// const getRootPlanet = (planets) => {
 
-  let root;
+//   let root;
   
-  for (let planet in planets) {
-    console.log('keys', Object.keys(planets[planet]))
-    if (Object.keys(planets[planet]).includes('parent') === false) {
-      console.log('finding root:', planets[planet]);
-      root = planets[planet];
-    }
-  }
-  console.log('root:', root)
-  return root;
+//   for (let planet in planets) {
+//     console.log('keys', Object.keys(planets[planet]))
+//     if (Object.keys(planets[planet]).includes('parent') === false) {
+//       console.log('finding root:', planets[planet]);
+//       root = planets[planet];
+//     }
+//   }
+//   console.log('root:', root)
+//   return root;
 
-};
+// };
 
-const rootPlanet = getRootPlanet(mappedPlanets);
-console.log('rootPlanet:', rootPlanet)
+// const rootPlanet = getRootPlanet(mappedPlanets);
+// console.log('rootPlanet:', rootPlanet)
 
 // use method to get total number of orbiters at the parent node
 
-rootPlanet.totalOrbiters();
+// rootPlanet.totalOrbiters();
