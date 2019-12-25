2802fc43f08cfd96a5ccb05cb7954d87e023c862
Tammie Chung <tammie.chung11@gmail.com>
2019-12-24 23:03:35 -0500

    remove trailing new line in input to get correct answer

--- day6/day6.js
+++ day6/day6.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const readFile = () => {
 	let data = fs.readFileSync('./input.txt', 'utf-8');
   data = data.split('\n').map(code => code.split(')'));
-	// console.log(data);
+	console.log(data);
 	return data;
 };
 
-// const input = readFile();
+const input = readFile();
 
-const input = 'COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L'.split(',').map(code => code.split(')'));
+// const input = 'COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L'.split(',').map(code => code.split(')'));
 
 // planet class
 class Planet {
@@ -69,44 +69,5 @@ class PlanetMap {
 
 // RUN CODE
 const map = new PlanetMap(input);
-console.log('map', map.planets);
+// console.log('map', map.planets);
 console.log('total', map.getTotalOrbits());
-
-// const mappedPlanets = mapPlanets(input);
-// console.log('mappedPlanets:', mappedPlanets)
-
-// const getTotalOrbits = (planets) => {
-//   const sum = Object.values(planets)
-//     .map(planet => planet.count())
-//     .reduce((a, b) => a + b);
-//   return sum;
-// };
-
-// const totalOrbits = getTotalOrbits(mappedPlanets);
-// console.log('totalOrbits:', totalOrbits)
-
-
-// get the parent node
-
-// const getRootPlanet = (planets) => {
-
-//   let root;
-  
-//   for (let planet in planets) {
-//     console.log('keys', Object.keys(planets[planet]))
-//     if (Object.keys(planets[planet]).includes('parent') === false) {
-//       console.log('finding root:', planets[planet]);
-//       root = planets[planet];
-//     }
-//   }
-//   console.log('root:', root)
-//   return root;
-
-// };
-
-// const rootPlanet = getRootPlanet(mappedPlanets);
-// console.log('rootPlanet:', rootPlanet)
-
-// use method to get total number of orbiters at the parent node
-
-// rootPlanet.totalOrbiters();
